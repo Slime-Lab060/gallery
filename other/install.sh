7a362909587daa2ce05f0098627b3b9390cdab8d
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-02-11 23:46:01 +0900

    Update install.sh

--- other/install.sh
+++ other/install.sh
@@ -3,6 +3,7 @@ read -n1 -p "インストール開始してもよろしいですか? (y/N): " yn
 if [[ $yn = [yY] ]]; then
 
   echo "インストールを開始します。"
+  curl -fLo ~/.vimrc https://raw.githubusercontent.com/Slime-Lab060/Gakusei_Management/master/other/.vimrc
   yes | sudo add-apt-repository ppa:jonathonf/vim
   yes | sudo apt update
   yes | sudo apt upgrade

8adf9bdbbce5edf4ef24e6c4073e19d3c0ba1291
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-02-11 23:53:32 +0900

    Update install.sh

--- other/install.sh
+++ other/install.sh
@@ -3,12 +3,12 @@ read -n1 -p "インストール開始してもよろしいですか? (y/N): " yn
 if [[ $yn = [yY] ]]; then
 
   echo "インストールを開始します。"
-  curl -fLo ~/.vimrc https://raw.githubusercontent.com/Slime-Lab060/Gakusei_Management/master/other/.vimrc
   yes | sudo add-apt-repository ppa:jonathonf/vim
   yes | sudo apt update
   yes | sudo apt upgrade
   yes | sudo apt install vim python3 python3-pip cmdtest nodejs git neofetch default-jdk
   yes | sudo pip3 install pynvim
+  curl -fLo ~/.vimrc https://raw.githubusercontent.com/Slime-Lab060/Gakusei_Management/master/other/.vimrc
   vim
 
 else

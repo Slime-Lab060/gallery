3f4bb78f385b3dbbc0b9703bd963cc53f5e97453
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-02-25 14:49:30 +0900

    Update install.sh

--- other/install.sh
+++ other/install.sh
@@ -9,6 +9,8 @@ if [[ $yn = [yY] ]]; then
   yes | sudo apt install vim python3 python3-pip cmdtest nodejs git neofetch default-jdk
   yes | sudo pip3 install pynvim
   curl -fLo ~/.vimrc https://raw.githubusercontent.com/Slime-Lab060/Gakusei_Management/master/other/.vimrc
+  curl -fLo ~/update.sh https://raw.githubusercontent.com/Slime-Lab060/Gakusei_Management/master/other/update.sh
+  curl -fLo ~/uninstall.sh  https://raw.githubusercontent.com/Slime-Lab060/Gakusei_Management/master/other/uninstall.sh
   vim
 
 else

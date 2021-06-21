b4d6d6c0124ca44f2d4192693863353c1ac964cd
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-06-21 23:09:27 +0900

    2020年6月作成

---
+++ gakusei/C_Sharp/Rensyu/Rensyu05/Rensyu05/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Rensyu05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKei = new System.Windows.Forms.Button();
+            this.txtAtai = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAns = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKei
+            // 
+            this.btnKei.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnKei.Location = new System.Drawing.Point(95, 78);
+            this.btnKei.Name = "btnKei";
+            this.btnKei.Size = new System.Drawing.Size(84, 50);
+            this.btnKei.TabIndex = 0;
+            this.btnKei.Text = "計算";
+            this.btnKei.UseVisualStyleBackColor = true;
+            this.btnKei.Click += new System.EventHandler(this.btnKei_Click);
+            // 
+            // txtAtai
+            // 
+            this.txtAtai.Font = new System.Drawing.Font("MS UI Gothic", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtAtai.Location = new System.Drawing.Point(12, 12);
+            this.txtAtai.Multiline = true;
+            this.txtAtai.Name = "txtAtai";
+            this.txtAtai.Size = new System.Drawing.Size(86, 43);
+            this.txtAtai.TabIndex = 1;
+            this.txtAtai.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(104, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 43);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "! =";
+            // 
+            // lblAns
+            // 
+            this.lblAns.AutoSize = true;
+            this.lblAns.Font = new System.Drawing.Font("MS UI Gothic", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblAns.Location = new System.Drawing.Point(173, 12);
+            this.lblAns.Name = "lblAns";
+            this.lblAns.Size = new System.Drawing.Size(0, 43);
+            this.lblAns.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 146);
+            this.Controls.Add(this.lblAns);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAtai);
+            this.Controls.Add(this.btnKei);
+            this.Name = "Form1";
+            this.Text = "階乗計算";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKei;
+        private System.Windows.Forms.TextBox txtAtai;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAns;
+    }
+}
+
---
+++ gakusei/C_Sharp/Rensyu/Rensyu05/Rensyu05/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rensyu05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKei_Click(object sender, EventArgs e)
+        {
+            int n;
+            int i=1;
+            int a = 1;
+
+            if (int.TryParse(txtAtai.Text, out n) == true){
+
+                while (n >= i)
+                {
+                    a *=  i;
+                    i++;
+                }
+                lblAns.Text = a.ToString();
+            }
+            else
+            {
+                MessageBox.Show("数字を入力してください");
+            }
+            /*
+            int Factorial(int n)
+            {
+                if (n == 0)
+                    return 1;
+                return n * Factorial(n - 1);
+            }
+            */
+        }
+    }
+}

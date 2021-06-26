2407d4e12751777187bd4a0db0fa00ab11160a29
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-06-26 20:14:12 +0900

    2020年6月作成

---
+++ gakusei/C_Sharp/Rensyu/Rensyu10/Rensyu10/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Rensyu10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstJpn = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKekka = new System.Windows.Forms.TextBox();
+            this.btnyaku = new System.Windows.Forms.Button();
+            this.lstEng = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstJpn
+            // 
+            this.lstJpn.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lstJpn.FormattingEnabled = true;
+            this.lstJpn.ItemHeight = 24;
+            this.lstJpn.Location = new System.Drawing.Point(12, 72);
+            this.lstJpn.Name = "lstJpn";
+            this.lstJpn.Size = new System.Drawing.Size(155, 268);
+            this.lstJpn.TabIndex = 0;
+            this.lstJpn.SelectedIndexChanged += new System.EventHandler(this.lstJpn_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(65, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "翻訳補助";
+            // 
+            // txtKekka
+            // 
+            this.txtKekka.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtKekka.Location = new System.Drawing.Point(195, 72);
+            this.txtKekka.Multiline = true;
+            this.txtKekka.Name = "txtKekka";
+            this.txtKekka.Size = new System.Drawing.Size(161, 34);
+            this.txtKekka.TabIndex = 2;
+            // 
+            // btnyaku
+            // 
+            this.btnyaku.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnyaku.Location = new System.Drawing.Point(196, 121);
+            this.btnyaku.Name = "btnyaku";
+            this.btnyaku.Size = new System.Drawing.Size(159, 40);
+            this.btnyaku.TabIndex = 3;
+            this.btnyaku.Text = "翻訳";
+            this.btnyaku.UseVisualStyleBackColor = true;
+            this.btnyaku.Click += new System.EventHandler(this.btnyaku_Click);
+            // 
+            // lstEng
+            // 
+            this.lstEng.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lstEng.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lstEng.FormattingEnabled = true;
+            this.lstEng.ItemHeight = 24;
+            this.lstEng.Location = new System.Drawing.Point(173, 72);
+            this.lstEng.Name = "lstEng";
+            this.lstEng.Size = new System.Drawing.Size(16, 268);
+            this.lstEng.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 364);
+            this.Controls.Add(this.lstEng);
+            this.Controls.Add(this.btnyaku);
+            this.Controls.Add(this.txtKekka);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstJpn);
+            this.Name = "Form1";
+            this.Text = "日本語→英語";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstJpn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKekka;
+        private System.Windows.Forms.Button btnyaku;
+        private System.Windows.Forms.ListBox lstEng;
+    }
+}
+
---
+++ gakusei/C_Sharp/Rensyu/Rensyu10/Rensyu10/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rensyu10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lstJpn.Items.Add("１月");
+            lstJpn.Items.Add("２月");
+            lstJpn.Items.Add("３月");
+            lstJpn.Items.Add("４月");
+            lstJpn.Items.Add("５月");
+            lstJpn.Items.Add("６月");
+            lstJpn.Items.Add("７月");
+            lstJpn.Items.Add("８月");
+            lstJpn.Items.Add("９月");
+            lstJpn.Items.Add("１０月");
+            lstJpn.Items.Add("１１月");
+            lstJpn.Items.Add("１２月");
+            lstEng.Items.Add("Ｊａｎｕａｒｙ");
+            lstEng.Items.Add("Ｆｅｂｒｕａｒｙ");
+            lstEng.Items.Add("Ｍａｒｃｈ");
+            lstEng.Items.Add("Ａｐｒｉｌ");
+            lstEng.Items.Add("Ｍａｙ");
+            lstEng.Items.Add("Ｊｕｎｅ");
+            lstEng.Items.Add("Ｊｕｌｙ");
+            lstEng.Items.Add("Ａｕｇｕｓｔ");
+            lstEng.Items.Add("Ｓｅｐｔｅｍｂｅｒ");
+            lstEng.Items.Add("Ｏｃｔｏｂｅｒ");
+            lstEng.Items.Add("Ｎｏｖｅｍｂｅｒ");
+            lstEng.Items.Add("Ｄｅｃｅｍｂｅｒ");
+
+        }
+
+        private void btnyaku_Click(object sender, EventArgs e)
+        {
+            if (lstJpn.SelectedItem == null)
+            {
+                MessageBox.Show("選択してください。");
+            }
+            else
+            {
+                int i = 0;
+                i = lstJpn.SelectedIndex;
+                txtKekka.Text = lstEng.Items[i].ToString();
+            }
+        }
+        public void data()
+        {
+        }
+
+        private void lstJpn_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}

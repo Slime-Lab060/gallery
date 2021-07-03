ea61bd587349eaffe00ff5e682922221ab5b9d5e
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-07-03 19:56:11 +0900

    2020年6月作成

---
+++ gakusei/C_Sharp/Rensyu/Rensyu12/Rensyu12/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Rensyu12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAns = new System.Windows.Forms.TextBox();
+            this.btnNum7 = new System.Windows.Forms.Button();
+            this.btnNum8 = new System.Windows.Forms.Button();
+            this.btnNum9 = new System.Windows.Forms.Button();
+            this.btnNum6 = new System.Windows.Forms.Button();
+            this.btnNum5 = new System.Windows.Forms.Button();
+            this.btnNum4 = new System.Windows.Forms.Button();
+            this.btnNum3 = new System.Windows.Forms.Button();
+            this.btnNum2 = new System.Windows.Forms.Button();
+            this.btnNum1 = new System.Windows.Forms.Button();
+            this.txtNum0 = new System.Windows.Forms.Button();
+            this.btnAc = new System.Windows.Forms.Button();
+            this.btnKigoWaru = new System.Windows.Forms.Button();
+            this.btnKigoKake = new System.Windows.Forms.Button();
+            this.btnKigoHiku = new System.Windows.Forms.Button();
+            this.btnKigoTasu = new System.Windows.Forms.Button();
+            this.btnWa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAns
+            // 
+            this.txtAns.Enabled = false;
+            this.txtAns.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtAns.Location = new System.Drawing.Point(12, 12);
+            this.txtAns.Multiline = true;
+            this.txtAns.Name = "txtAns";
+            this.txtAns.Size = new System.Drawing.Size(324, 43);
+            this.txtAns.TabIndex = 0;
+            // 
+            // btnNum7
+            // 
+            this.btnNum7.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNum7.Location = new System.Drawing.Point(12, 61);
+            this.btnNum7.Name = "btnNum7";
+            this.btnNum7.Size = new System.Drawing.Size(60, 60);
+            this.btnNum7.TabIndex = 1;
+            this.btnNum7.Text = "7";
+            this.btnNum7.UseVisualStyleBackColor = true;
+            this.btnNum7.Click += new System.EventHandler(this.test);
+            // 
+            // btnNum8
+            // 
+            this.btnNum8.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNum8.Location = new System.Drawing.Point(78, 61);
+            this.btnNum8.Name = "btnNum8";
+            this.btnNum8.Size = new System.Drawing.Size(60, 60);
+            this.btnNum8.TabIndex = 2;
+            this.btnNum8.Text = "8";
+            this.btnNum8.UseVisualStyleBackColor = true;
+            this.btnNum8.Click += new System.EventHandler(this.test);
+            // 
+            // btnNum9
+            // 
+            this.btnNum9.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNum9.Location = new System.Drawing.Point(144, 61);
+            this.btnNum9.Name = "btnNum9";
+            this.btnNum9.Size = new System.Drawing.Size(60, 60);
+            this.btnNum9.TabIndex = 3;
+            this.btnNum9.Text = "9";
+            this.btnNum9.UseVisualStyleBackColor = true;
+            this.btnNum9.Click += new System.EventHandler(this.test);
+            // 
+            // btnNum6
+            // 
+            this.btnNum6.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNum6.Location = new System.Drawing.Point(144, 127);
+            this.btnNum6.Name = "btnNum6";
+            this.btnNum6.Size = new System.Drawing.Size(60, 60);
+            this.btnNum6.TabIndex = 6;
+            this.btnNum6.Text = "6";
+            this.btnNum6.UseVisualStyleBackColor = true;
+            this.btnNum6.Click += new System.EventHandler(this.test);
+            // 
+            // btnNum5
+            // 
+            this.btnNum5.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNum5.Location = new System.Drawing.Point(78, 127);
+            this.btnNum5.Name = "btnNum5";
+            this.btnNum5.Size = new System.Drawing.Size(60, 60);
+            this.btnNum5.TabIndex = 5;
+            this.btnNum5.Text = "5";
+            this.btnNum5.UseVisualStyleBackColor = true;
+            this.btnNum5.Click += new System.EventHandler(this.test);
+            // 
+            // btnNum4
+            // 
+            this.btnNum4.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNum4.Location = new System.Drawing.Point(12, 127);
+            this.btnNum4.Name = "btnNum4";
+            this.btnNum4.Size = new System.Drawing.Size(60, 60);
+            this.btnNum4.TabIndex = 4;
+            this.btnNum4.Text = "4";
+            this.btnNum4.UseVisualStyleBackColor = true;
+            this.btnNum4.Click += new System.EventHandler(this.test);
+            // 
+            // btnNum3
+            // 
+            this.btnNum3.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNum3.Location = new System.Drawing.Point(144, 193);
+            this.btnNum3.Name = "btnNum3";
+            this.btnNum3.Size = new System.Drawing.Size(60, 60);
+            this.btnNum3.TabIndex = 9;
+            this.btnNum3.Text = "3";
+            this.btnNum3.UseVisualStyleBackColor = true;
+            this.btnNum3.Click += new System.EventHandler(this.test);
+            // 
+            // btnNum2
+            // 
+            this.btnNum2.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNum2.Location = new System.Drawing.Point(78, 193);
+            this.btnNum2.Name = "btnNum2";
+            this.btnNum2.Size = new System.Drawing.Size(60, 60);
+            this.btnNum2.TabIndex = 8;
+            this.btnNum2.Text = "2";
+            this.btnNum2.UseVisualStyleBackColor = true;
+            this.btnNum2.Click += new System.EventHandler(this.test);
+            // 
+            // btnNum1
+            // 
+            this.btnNum1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNum1.Location = new System.Drawing.Point(12, 193);
+            this.btnNum1.Name = "btnNum1";
+            this.btnNum1.Size = new System.Drawing.Size(60, 60);
+            this.btnNum1.TabIndex = 1;
+            this.btnNum1.Text = "1";
+            this.btnNum1.UseVisualStyleBackColor = true;
+            this.btnNum1.Click += new System.EventHandler(this.test);
+            // 
+            // txtNum0
+            // 
+            this.txtNum0.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtNum0.Location = new System.Drawing.Point(12, 259);
+            this.txtNum0.Name = "txtNum0";
+            this.txtNum0.Size = new System.Drawing.Size(192, 60);
+            this.txtNum0.TabIndex = 10;
+            this.txtNum0.Text = "0";
+            this.txtNum0.UseVisualStyleBackColor = true;
+            this.txtNum0.Click += new System.EventHandler(this.test);
+            // 
+            // btnAc
+            // 
+            this.btnAc.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnAc.Location = new System.Drawing.Point(210, 61);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(126, 60);
+            this.btnAc.TabIndex = 11;
+            this.btnAc.Text = "AC";
+            this.btnAc.UseVisualStyleBackColor = true;
+            this.btnAc.Click += new System.EventHandler(this.btnAc_Click);
+            // 
+            // btnKigoWaru
+            // 
+            this.btnKigoWaru.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnKigoWaru.Location = new System.Drawing.Point(276, 193);
+            this.btnKigoWaru.Name = "btnKigoWaru";
+            this.btnKigoWaru.Size = new System.Drawing.Size(60, 60);
+            this.btnKigoWaru.TabIndex = 15;
+            this.btnKigoWaru.Text = "÷";
+            this.btnKigoWaru.UseVisualStyleBackColor = true;
+            this.btnKigoWaru.Click += new System.EventHandler(this.btnKigoTasu_Click_1);
+            // 
+            // btnKigoKake
+            // 
+            this.btnKigoKake.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnKigoKake.Location = new System.Drawing.Point(210, 193);
+            this.btnKigoKake.Name = "btnKigoKake";
+            this.btnKigoKake.Size = new System.Drawing.Size(60, 60);
+            this.btnKigoKake.TabIndex = 14;
+            this.btnKigoKake.Text = "×";
+            this.btnKigoKake.UseVisualStyleBackColor = true;
+            this.btnKigoKake.Click += new System.EventHandler(this.btnKigoTasu_Click_1);
+            // 
+            // btnKigoHiku
+            // 
+            this.btnKigoHiku.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnKigoHiku.Location = new System.Drawing.Point(276, 127);
+            this.btnKigoHiku.Name = "btnKigoHiku";
+            this.btnKigoHiku.Size = new System.Drawing.Size(60, 60);
+            this.btnKigoHiku.TabIndex = 13;
+            this.btnKigoHiku.Text = "－";
+            this.btnKigoHiku.UseVisualStyleBackColor = true;
+            this.btnKigoHiku.Click += new System.EventHandler(this.btnKigoTasu_Click_1);
+            // 
+            // btnKigoTasu
+            // 
+            this.btnKigoTasu.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnKigoTasu.Location = new System.Drawing.Point(210, 127);
+            this.btnKigoTasu.Name = "btnKigoTasu";
+            this.btnKigoTasu.Size = new System.Drawing.Size(60, 60);
+            this.btnKigoTasu.TabIndex = 12;
+            this.btnKigoTasu.Text = "＋";
+            this.btnKigoTasu.UseVisualStyleBackColor = true;
+            this.btnKigoTasu.Click += new System.EventHandler(this.btnKigoTasu_Click_1);
+            // 
+            // btnWa
+            // 
+            this.btnWa.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnWa.Location = new System.Drawing.Point(210, 259);
+            this.btnWa.Name = "btnWa";
+            this.btnWa.Size = new System.Drawing.Size(126, 60);
+            this.btnWa.TabIndex = 16;
+            this.btnWa.Text = "＝";
+            this.btnWa.UseVisualStyleBackColor = true;
+            this.btnWa.Click += new System.EventHandler(this.btnKigoTasu_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 332);
+            this.Controls.Add(this.btnWa);
+            this.Controls.Add(this.btnKigoWaru);
+            this.Controls.Add(this.btnKigoKake);
+            this.Controls.Add(this.btnKigoHiku);
+            this.Controls.Add(this.btnKigoTasu);
+            this.Controls.Add(this.btnAc);
+            this.Controls.Add(this.txtNum0);
+            this.Controls.Add(this.btnNum3);
+            this.Controls.Add(this.btnNum2);
+            this.Controls.Add(this.btnNum1);
+            this.Controls.Add(this.btnNum6);
+            this.Controls.Add(this.btnNum5);
+            this.Controls.Add(this.btnNum4);
+            this.Controls.Add(this.btnNum9);
+            this.Controls.Add(this.btnNum8);
+            this.Controls.Add(this.btnNum7);
+            this.Controls.Add(this.txtAns);
+            this.Name = "Form1";
+            this.Text = "電卓";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAns;
+        private System.Windows.Forms.Button btnNum7;
+        private System.Windows.Forms.Button btnNum8;
+        private System.Windows.Forms.Button btnNum9;
+        private System.Windows.Forms.Button btnNum6;
+        private System.Windows.Forms.Button btnNum5;
+        private System.Windows.Forms.Button btnNum4;
+        private System.Windows.Forms.Button btnNum3;
+        private System.Windows.Forms.Button btnNum2;
+        private System.Windows.Forms.Button btnNum1;
+        private System.Windows.Forms.Button txtNum0;
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.Button btnKigoWaru;
+        private System.Windows.Forms.Button btnKigoKake;
+        private System.Windows.Forms.Button btnKigoHiku;
+        private System.Windows.Forms.Button btnKigoTasu;
+        private System.Windows.Forms.Button btnWa;
+    }
+}
+
---
+++ gakusei/C_Sharp/Rensyu/Rensyu12/Rensyu12/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rensyu12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string Input_str = "";  // 入力された数字
+        double Result = 0;      // 計算結果
+        string Operator = null; // 押された演算子
+
+        private void btnAc_Click(object sender, EventArgs e)
+        {
+            //ACが押されたらリセット値をリセットする
+            txtAns.Text = "";
+            Input_str = "";  
+            Result = 0;      
+            Operator = null; 
+
+        }
+
+        private void test(object sender, EventArgs e)
+        {
+            //0～9のボタンが押されたら値を代入する
+            Button btn = (Button)sender;
+            String text = btn.Text;
+            Input_str += text;
+            txtAns.Text = Input_str;
+        }
+
+        private void btnKigoTasu_Click_1(object sender, EventArgs e)
+        {
+            double num1 = Result;   // 現在の結果
+            double num2;            // 入力された文字
+
+            // 入力された文字が空欄なら、計算をスキップする
+            if (Input_str != "")
+            {
+                // 入力した文字を数字に変換
+                num2 = double.Parse(Input_str);
+
+                // 四則計算
+                if (Operator == "＋")
+                    Result = num1 + num2;
+                if (Operator == "－")
+                    Result = num1 - num2;
+                if (Operator == "×")
+                    Result = num1 * num2;
+                if (Operator == "÷")
+                    if (num2 == 0)
+                    {
+                        MessageBox.Show("0で割ることはできません");
+                    }
+                    else
+                    {
+                        Result = num1 / num2;
+                    }
+
+                // 演算子を押されていなかった場合、入力されている文字をそのまま結果扱いにする
+                if (Operator == null)
+                    Result = num2;
+            }
+
+            // 画面に計算結果を表示する
+            txtAns.Text = Result.ToString();
+
+            // 今入力されている数字をリセットする
+            Input_str = "";
+
+            // 演算子をOperator変数に入れる
+            Button btn = (Button)sender;
+
+            String kigou = btn.Text;
+            Operator = btn.Text;
+
+            //=が押されたらリセットする
+            if (Operator == "=")
+                Operator = "";
+        }
+    }
+}

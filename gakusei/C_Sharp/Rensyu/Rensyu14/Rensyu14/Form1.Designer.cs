038f4f978506af7653598dd19bf8ca5a957bac6a
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-07-11 12:37:48 +0900

    2020年6月作成

---
+++ gakusei/C_Sharp/Rensyu/Rensyu14/Rensyu14/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Rensyu14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTbl8 = new System.Windows.Forms.TextBox();
+            this.txtTbl7 = new System.Windows.Forms.TextBox();
+            this.txtTbl6 = new System.Windows.Forms.TextBox();
+            this.txtTbl5 = new System.Windows.Forms.TextBox();
+            this.txtTbl4 = new System.Windows.Forms.TextBox();
+            this.txtTbl3 = new System.Windows.Forms.TextBox();
+            this.txtTbl2 = new System.Windows.Forms.TextBox();
+            this.txtTbl1 = new System.Windows.Forms.TextBox();
+            this.txtTbl0 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.frmKekka = new System.Windows.Forms.GroupBox();
+            this.optNashi = new System.Windows.Forms.RadioButton();
+            this.optAri = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.処理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.並列処理ACtrlAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.テーブル入力TCtrlTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.検索入力NCtrlNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.並列処理SCtrlSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.検索結果SCtrlSToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.削除処理CToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.frmKekka.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTbl8);
+            this.groupBox1.Controls.Add(this.txtTbl7);
+            this.groupBox1.Controls.Add(this.txtTbl6);
+            this.groupBox1.Controls.Add(this.txtTbl5);
+            this.groupBox1.Controls.Add(this.txtTbl4);
+            this.groupBox1.Controls.Add(this.txtTbl3);
+            this.groupBox1.Controls.Add(this.txtTbl2);
+            this.groupBox1.Controls.Add(this.txtTbl1);
+            this.groupBox1.Controls.Add(this.txtTbl0);
+            this.groupBox1.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(481, 97);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "データテーブル";
+            // 
+            // txtTbl8
+            // 
+            this.txtTbl8.Enabled = false;
+            this.txtTbl8.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtTbl8.Location = new System.Drawing.Point(414, 35);
+            this.txtTbl8.Multiline = true;
+            this.txtTbl8.Name = "txtTbl8";
+            this.txtTbl8.Size = new System.Drawing.Size(45, 45);
+            this.txtTbl8.TabIndex = 8;
+            this.txtTbl8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTbl7
+            // 
+            this.txtTbl7.Enabled = false;
+            this.txtTbl7.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtTbl7.Location = new System.Drawing.Point(363, 35);
+            this.txtTbl7.Multiline = true;
+            this.txtTbl7.Name = "txtTbl7";
+            this.txtTbl7.Size = new System.Drawing.Size(45, 45);
+            this.txtTbl7.TabIndex = 7;
+            this.txtTbl7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTbl6
+            // 
+            this.txtTbl6.Enabled = false;
+            this.txtTbl6.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtTbl6.Location = new System.Drawing.Point(312, 35);
+            this.txtTbl6.Multiline = true;
+            this.txtTbl6.Name = "txtTbl6";
+            this.txtTbl6.Size = new System.Drawing.Size(45, 45);
+            this.txtTbl6.TabIndex = 6;
+            this.txtTbl6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTbl5
+            // 
+            this.txtTbl5.Enabled = false;
+            this.txtTbl5.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtTbl5.Location = new System.Drawing.Point(261, 35);
+            this.txtTbl5.Multiline = true;
+            this.txtTbl5.Name = "txtTbl5";
+            this.txtTbl5.Size = new System.Drawing.Size(45, 45);
+            this.txtTbl5.TabIndex = 5;
+            this.txtTbl5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTbl4
+            // 
+            this.txtTbl4.Enabled = false;
+            this.txtTbl4.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtTbl4.Location = new System.Drawing.Point(210, 35);
+            this.txtTbl4.Multiline = true;
+            this.txtTbl4.Name = "txtTbl4";
+            this.txtTbl4.Size = new System.Drawing.Size(45, 45);
+            this.txtTbl4.TabIndex = 4;
+            this.txtTbl4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTbl3
+            // 
+            this.txtTbl3.Enabled = false;
+            this.txtTbl3.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtTbl3.Location = new System.Drawing.Point(159, 35);
+            this.txtTbl3.Multiline = true;
+            this.txtTbl3.Name = "txtTbl3";
+            this.txtTbl3.Size = new System.Drawing.Size(45, 45);
+            this.txtTbl3.TabIndex = 3;
+            this.txtTbl3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTbl2
+            // 
+            this.txtTbl2.Enabled = false;
+            this.txtTbl2.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtTbl2.Location = new System.Drawing.Point(108, 35);
+            this.txtTbl2.Multiline = true;
+            this.txtTbl2.Name = "txtTbl2";
+            this.txtTbl2.Size = new System.Drawing.Size(45, 45);
+            this.txtTbl2.TabIndex = 2;
+            this.txtTbl2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTbl1
+            // 
+            this.txtTbl1.Enabled = false;
+            this.txtTbl1.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtTbl1.Location = new System.Drawing.Point(57, 35);
+            this.txtTbl1.Multiline = true;
+            this.txtTbl1.Name = "txtTbl1";
+            this.txtTbl1.Size = new System.Drawing.Size(45, 45);
+            this.txtTbl1.TabIndex = 1;
+            this.txtTbl1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTbl0
+            // 
+            this.txtTbl0.Enabled = false;
+            this.txtTbl0.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtTbl0.Location = new System.Drawing.Point(6, 35);
+            this.txtTbl0.Multiline = true;
+            this.txtTbl0.Name = "txtTbl0";
+            this.txtTbl0.Size = new System.Drawing.Size(45, 45);
+            this.txtTbl0.TabIndex = 0;
+            this.txtTbl0.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.label1.Location = new System.Drawing.Point(20, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "検索する値";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Enabled = false;
+            this.txtNum.Font = new System.Drawing.Font("MS UI Gothic", 24F);
+            this.txtNum.Location = new System.Drawing.Point(146, 144);
+            this.txtNum.Multiline = true;
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(70, 42);
+            this.txtNum.TabIndex = 3;
+            this.txtNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmKekka
+            // 
+            this.frmKekka.Controls.Add(this.optNashi);
+            this.frmKekka.Controls.Add(this.optAri);
+            this.frmKekka.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.frmKekka.Location = new System.Drawing.Point(242, 137);
+            this.frmKekka.Name = "frmKekka";
+            this.frmKekka.Size = new System.Drawing.Size(229, 60);
+            this.frmKekka.TabIndex = 4;
+            this.frmKekka.TabStop = false;
+            this.frmKekka.Text = "検索結果";
+            // 
+            // optNashi
+            // 
+            this.optNashi.AutoSize = true;
+            this.optNashi.Enabled = false;
+            this.optNashi.Location = new System.Drawing.Point(133, 29);
+            this.optNashi.Name = "optNashi";
+            this.optNashi.Size = new System.Drawing.Size(66, 28);
+            this.optNashi.TabIndex = 1;
+            this.optNashi.Text = "なし";
+            this.optNashi.UseVisualStyleBackColor = true;
+            // 
+            // optAri
+            // 
+            this.optAri.AutoSize = true;
+            this.optAri.Checked = true;
+            this.optAri.Enabled = false;
+            this.optAri.Location = new System.Drawing.Point(16, 29);
+            this.optAri.Name = "optAri";
+            this.optAri.Size = new System.Drawing.Size(63, 28);
+            this.optAri.TabIndex = 0;
+            this.optAri.TabStop = true;
+            this.optAri.Text = "あり";
+            this.optAri.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.処理ToolStripMenuItem,
+            this.削除処理CToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(505, 24);
+            this.menuStrip1.TabIndex = 5;
+            // 
+            // 処理ToolStripMenuItem
+            // 
+            this.処理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.並列処理ACtrlAToolStripMenuItem,
+            this.並列処理SCtrlSToolStripMenuItem,
+            this.検索結果SCtrlSToolStripMenuItem1});
+            this.処理ToolStripMenuItem.Name = "処理ToolStripMenuItem";
+            this.処理ToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.処理ToolStripMenuItem.Text = "入力処理(I)";
+            // 
+            // 並列処理ACtrlAToolStripMenuItem
+            // 
+            this.並列処理ACtrlAToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.テーブル入力TCtrlTToolStripMenuItem,
+            this.検索入力NCtrlNToolStripMenuItem});
+            this.並列処理ACtrlAToolStripMenuItem.Name = "並列処理ACtrlAToolStripMenuItem";
+            this.並列処理ACtrlAToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.並列処理ACtrlAToolStripMenuItem.Text = "入力処理(I)";
+            // 
+            // テーブル入力TCtrlTToolStripMenuItem
+            // 
+            this.テーブル入力TCtrlTToolStripMenuItem.Name = "テーブル入力TCtrlTToolStripMenuItem";
+            this.テーブル入力TCtrlTToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.テーブル入力TCtrlTToolStripMenuItem.Text = "テーブル入力(T) Ctrl+T";
+            this.テーブル入力TCtrlTToolStripMenuItem.Click += new System.EventHandler(this.テーブル入力TCtrlTToolStripMenuItem_Click);
+            // 
+            // 検索入力NCtrlNToolStripMenuItem
+            // 
+            this.検索入力NCtrlNToolStripMenuItem.Name = "検索入力NCtrlNToolStripMenuItem";
+            this.検索入力NCtrlNToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.検索入力NCtrlNToolStripMenuItem.Text = "検索値入力(N) Ctrl+N";
+            this.検索入力NCtrlNToolStripMenuItem.Click += new System.EventHandler(this.検索入力NCtrlNToolStripMenuItem_Click);
+            // 
+            // 並列処理SCtrlSToolStripMenuItem
+            // 
+            this.並列処理SCtrlSToolStripMenuItem.Enabled = false;
+            this.並列処理SCtrlSToolStripMenuItem.Name = "並列処理SCtrlSToolStripMenuItem";
+            this.並列処理SCtrlSToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.並列処理SCtrlSToolStripMenuItem.Text = "並列処理(S) Ctrl+A";
+            this.並列処理SCtrlSToolStripMenuItem.Click += new System.EventHandler(this.並列結果SCtrlSToolStripMenuItem_Click);
+            // 
+            // 検索結果SCtrlSToolStripMenuItem1
+            // 
+            this.検索結果SCtrlSToolStripMenuItem1.Enabled = false;
+            this.検索結果SCtrlSToolStripMenuItem1.Name = "検索結果SCtrlSToolStripMenuItem1";
+            this.検索結果SCtrlSToolStripMenuItem1.Size = new System.Drawing.Size(173, 22);
+            this.検索結果SCtrlSToolStripMenuItem1.Text = "検索結果(S) Ctrl+S";
+            this.検索結果SCtrlSToolStripMenuItem1.Click += new System.EventHandler(this.検索結果SCtrlSToolStripMenuItem1_Click);
+            // 
+            // 削除処理CToolStripMenuItem
+            // 
+            this.削除処理CToolStripMenuItem.Enabled = false;
+            this.削除処理CToolStripMenuItem.Name = "削除処理CToolStripMenuItem";
+            this.削除処理CToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.削除処理CToolStripMenuItem.Text = "削除処理(C)";
+            this.削除処理CToolStripMenuItem.Click += new System.EventHandler(this.削除処理CToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 209);
+            this.Controls.Add(this.frmKekka);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "二分探索";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.frmKekka.ResumeLayout(false);
+            this.frmKekka.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTbl8;
+        private System.Windows.Forms.TextBox txtTbl7;
+        private System.Windows.Forms.TextBox txtTbl6;
+        private System.Windows.Forms.TextBox txtTbl5;
+        private System.Windows.Forms.TextBox txtTbl3;
+        private System.Windows.Forms.TextBox txtTbl2;
+        private System.Windows.Forms.TextBox txtTbl1;
+        private System.Windows.Forms.TextBox txtTbl0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.GroupBox frmKekka;
+        private System.Windows.Forms.RadioButton optNashi;
+        private System.Windows.Forms.RadioButton optAri;
+        private System.Windows.Forms.TextBox txtTbl4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 処理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 並列処理ACtrlAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem テーブル入力TCtrlTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 検索入力NCtrlNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 並列処理SCtrlSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 検索結果SCtrlSToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 削除処理CToolStripMenuItem;
+    }
+}
+
---
+++ gakusei/C_Sharp/Rensyu/Rensyu14/Rensyu14/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rensyu14
+{
+
+    public partial class Form1 : Form
+    {
+        int[] array = new int[9];
+        int[] sarch = new int[9];
+        int temp = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 削除処理CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtTbl0.Text = "";
+            txtTbl1.Text = "";
+            txtTbl2.Text = "";
+            txtTbl3.Text = "";
+            txtTbl4.Text = "";
+            txtTbl5.Text = "";
+            txtTbl6.Text = "";
+            txtTbl7.Text = "";
+            txtTbl8.Text = "";
+
+
+        }
+
+        private void テーブル入力TCtrlTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtTbl0.Enabled = true;
+            txtTbl1.Enabled = true;
+            txtTbl2.Enabled = true;
+            txtTbl3.Enabled = true;
+            txtTbl4.Enabled = true;
+            txtTbl5.Enabled = true;
+            txtTbl6.Enabled = true;
+            txtTbl7.Enabled = true;
+            txtTbl8.Enabled = true;
+            並列処理SCtrlSToolStripMenuItem.Enabled = true;
+            検索結果SCtrlSToolStripMenuItem1.Enabled = true;
+            削除処理CToolStripMenuItem.Enabled = true;
+            txtNum.Enabled = false;
+
+        }
+
+        private void 並列結果SCtrlSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            array[0] = int.Parse(txtTbl0.Text);
+            array[1] = int.Parse(txtTbl1.Text);
+            array[2] = int.Parse(txtTbl2.Text);
+            array[3] = int.Parse(txtTbl3.Text);
+            array[4] = int.Parse(txtTbl4.Text);
+            array[5] = int.Parse(txtTbl5.Text);
+            array[6] = int.Parse(txtTbl6.Text);
+            array[7] = int.Parse(txtTbl7.Text);
+            array[8] = int.Parse(txtTbl8.Text);
+
+            /*隣接交換法*/
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            txtTbl0.Text = array[0].ToString();
+            txtTbl1.Text = array[1].ToString();
+            txtTbl2.Text = array[2].ToString();
+            txtTbl3.Text = array[3].ToString();
+            txtTbl4.Text = array[4].ToString();
+            txtTbl5.Text = array[5].ToString();
+            txtTbl6.Text = array[6].ToString();
+            txtTbl7.Text = array[7].ToString();
+            txtTbl8.Text = array[8].ToString();
+            txtTbl0.Enabled = false;
+            txtTbl1.Enabled = false;
+            txtTbl2.Enabled = false;
+            txtTbl3.Enabled = false;
+            txtTbl4.Enabled = false;
+            txtTbl5.Enabled = false;
+            txtTbl6.Enabled = false;
+            txtTbl7.Enabled = false;
+            txtTbl8.Enabled = false;
+            txtNum.Enabled = false;
+        }
+
+        private void 検索入力NCtrlNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtTbl0.Enabled = false;
+            txtTbl1.Enabled = false;
+            txtTbl2.Enabled = false;
+            txtTbl3.Enabled = false;
+            txtTbl4.Enabled = false;
+            txtTbl5.Enabled = false;
+            txtTbl6.Enabled = false;
+            txtTbl7.Enabled = false;
+            txtTbl8.Enabled = false;
+            txtNum.Enabled = true;
+        }
+
+        private void 検索結果SCtrlSToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            txtTbl0.BackColor = Color.White;
+            txtTbl1.BackColor = Color.White;
+            txtTbl2.BackColor = Color.White;
+            txtTbl3.BackColor = Color.White;
+            txtTbl4.BackColor = Color.White;
+            txtTbl5.BackColor = Color.White;
+            txtTbl6.BackColor = Color.White;
+            txtTbl7.BackColor = Color.White;
+            txtTbl8.BackColor = Color.White;
+
+            int mid=0;
+            int pos = -1;
+            int poss = 0;
+            int low = 0;
+            int up = sarch.Length - 1;
+            sarch[0] = int.Parse(txtTbl0.Text);
+            sarch[1] = int.Parse(txtTbl1.Text);
+            sarch[2] = int.Parse(txtTbl2.Text);
+            sarch[3] = int.Parse(txtTbl3.Text);
+            sarch[4] = int.Parse(txtTbl4.Text);
+            sarch[5] = int.Parse(txtTbl5.Text);
+            sarch[6] = int.Parse(txtTbl6.Text);
+            sarch[7] = int.Parse(txtTbl7.Text);
+            sarch[8] = int.Parse(txtTbl8.Text);
+            int value = int.Parse(txtNum.Text);
+
+            while (low <= up)
+            {
+                mid = (low + up) / 2;
+                if (sarch[mid] == value)
+                {
+                    pos = sarch[mid];
+                    poss = mid;
+                    result(mid);
+                    break;
+                    
+                    
+                }
+                else if (sarch[mid] < value)
+                {
+                    low = mid + 1;
+                }
+                else if (sarch[mid] > value)
+                {
+                    up = mid - 1;
+                }
+            }
+            if (pos < 0)
+            {
+                System.Diagnostics.Debug.WriteLine("見つかりません");
+                optNashi.Checked = true;
+            }
+            else
+            {
+            }
+        }
+        private void result(int pos)
+        {
+            //System.Diagnostics.Debug.WriteLine("見つかりました: " + pos + " " + poss);
+            optAri.Checked = true;
+
+            if (pos == 0)
+                txtTbl0.BackColor = Color.Black;
+            if (pos == 1)
+                txtTbl1.BackColor = Color.Black;
+            if (pos == 2)
+                txtTbl2.BackColor = Color.Black;
+            if (pos == 3)
+                txtTbl3.BackColor = Color.Black;
+            if (pos == 4)
+                txtTbl4.BackColor = Color.Black;
+            if (pos == 5)
+                txtTbl5.BackColor = Color.Black;
+            if (pos == 6)
+                txtTbl6.BackColor = Color.Black;
+            if (pos == 7)
+                txtTbl7.BackColor = Color.Black;
+            if (pos == 8)
+                txtTbl8.BackColor = Color.Black;
+        }
+    }
+}

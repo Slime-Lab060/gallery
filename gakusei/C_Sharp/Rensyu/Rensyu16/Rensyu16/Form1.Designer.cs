117fd0c8f2ee1daf572feef60ab1d68f8cad441b
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-07-15 18:06:00 +0900

    2020年6月作成

---
+++ gakusei/C_Sharp/Rensyu/Rensyu16/Rensyu16/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Rensyu16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtDisp = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtDisp
+            // 
+            this.txtDisp.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.txtDisp.Location = new System.Drawing.Point(12, 12);
+            this.txtDisp.Multiline = true;
+            this.txtDisp.Name = "txtDisp";
+            this.txtDisp.Size = new System.Drawing.Size(329, 41);
+            this.txtDisp.TabIndex = 0;
+            this.txtDisp.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.btnStart.Location = new System.Drawing.Point(13, 65);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(161, 38);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.btnStop.Location = new System.Drawing.Point(180, 65);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(161, 38);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("MS UI Gothic", 18F);
+            this.btnClear.Location = new System.Drawing.Point(93, 109);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(161, 38);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 157);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtDisp);
+            this.Name = "Form1";
+            this.Text = "ストップウォッチ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisp;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ gakusei/C_Sharp/Rensyu/Rensyu16/Rensyu16/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rensyu16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stopwatch Stopwatch = new Stopwatch();
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Stopwatch.Start();
+            timer1.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Stopwatch.Stop();
+            timer1.Enabled = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDisp.Text = "";
+            Stopwatch.Reset();
+            timer1.Enabled = false;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtDisp.Text = (Stopwatch.Elapsed).ToString(@"ss");
+        }
+
+    }
+}

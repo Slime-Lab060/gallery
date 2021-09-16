d6a957870bb1ea8d2ca5e331dffd787e4fa0c52d
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-09-16 18:08:17 +0900

    Update install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -4,6 +4,7 @@ if [[ $yn = [yY] ]]; then
 
   echo "インストールを開始します。"
   yes | sudo add-apt-repository ppa:jonathonf/vim
+  yes | sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash
   yes | sudo apt update
   yes | sudo apt upgrade
   yes | sudo apt install vim python3 python3-pip cmdtest nodejs git neofetch default-jdk

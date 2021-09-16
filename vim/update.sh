5e2d53f530e5bc9859f95bf49f22cd3667376da8
Slime Lab <68540736+Slime-Lab060@users.noreply.github.com>
2021-09-16 17:32:26 +0900

    Update update.sh

--- vim/update.sh
+++ vim/update.sh
@@ -3,7 +3,7 @@ read -n1 -p "アップデートを開始してもよろしいですか? (y/N): "
 if [[ $yn = [yY] ]]; then
 
   echo "アップデートを開始します。"
-  curl -fLo ~/.vimrc https://raw.githubusercontent.com/Slime-Lab060/Gakusei_Management/master/other/.vimrc
+  curl -fLo ~/.vimrc https://raw.githubusercontent.com/Slime-Lab060/Gakusei_Management/master/vim/.vimrc
   echo "アップデート完了しました。"
 
 else
